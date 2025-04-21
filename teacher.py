51e9023841ef342e94564e74200dd4d6465fdc15
ALex03-HELP-ME <alexxxlin168@gmail.com>
2025-04-21 09:25:30 -0400

    kkk

--- teacher.py
+++ teacher.py
@@ -1,10 +1,22 @@
 import json
 
-enter = input("what is the question?")
 
-enter1 = input("What is the answer?")
+class card:
+    def __init__(self, question, answer):
+        self.enter = enter
+
+        self.enter1 = 
+
+    def display_info(self):
+        return f"{self.enter} {self.enter1}"
+    
+    def to_dict(info):
+        return {"question": info.enter, "answer": info.enter1}
 
 
-with open("cars.json", "w") as file:
-    json.dump(fash_data, file, indent=4)
+x =input("what is the question?")
 
+y= input("What is the answer?")
+
+newCard = card(x,y)
+print(newCard.display_info)
