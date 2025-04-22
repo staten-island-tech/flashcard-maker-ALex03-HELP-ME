c4ef5c1b1d0789563b94ed3890a415bd0b05c4b8
ALex03-HELP-ME <alexxxlin168@gmail.com>
2025-04-22 09:24:43 -0400

    Update teacher.py

--- teacher.py
+++ teacher.py
@@ -3,9 +3,9 @@ import json
 
 class card:
     def __init__(self, question, answer):
-        self.enter = x
+        self.enter = question
 
-        self.enter1 = y
+        self.enter1 = answer
 
     def display_info(self):
         return f"{self.enter} {self.enter1}"
@@ -14,9 +14,12 @@ class card:
         return {"question": info.enter, "answer": info.enter1}
 
 
-x =input("what is the question?")
+x =input("what is the question?") 
 
 y= input("What is the answer?")
 
 newCard = card(x,y)
 print(newCard.display_info)
+
+with open("hi.json", "w") as json_file:
+    json.dump(class, json_file, indent=4)
